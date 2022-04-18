baf8e84657ec2b5c8cadd9d0a5e825ec81ff3c97
XCL <xclancn@outlook.com>
2022-04-18 22:22:25 +0800

    style: layout the page using Row

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,12 +1,39 @@
 import { useAuth } from "context/auth-context"
 import { ProjectListScreen } from "screens/project-list"
+import styled from "@emotion/styled"
+import { Row } from "components/lib"
 
 export const AuthenticatedApp = () => {
   const { logout } = useAuth()
   return (
-    <div>
-      <ProjectListScreen />
-      <button onClick={logout}>登出</button>
-    </div>
+    <Container>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <h2>logo</h2>
+          <h2>项目</h2>
+          <h2>用户</h2>
+        </HeaderLeft>
+        <HeaderRight>
+          <button onClick={logout}>登出</button>
+        </HeaderRight>
+      </Header>
+      <Main>
+        <ProjectListScreen />
+      </Main>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr;
+`
+const Header = styled(Row)``
+
+const HeaderLeft = styled(Row)``
+
+const HeaderRight = styled.div``
+
+const Main = styled.main`
+  height: calc(100vh - 6rem);
+`
